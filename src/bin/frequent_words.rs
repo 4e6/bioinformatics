50a1025cc10f19d93bfe13c02ac29451748fa04c
Dmitry Bushev <bushevdv@gmail.com>
2016-08-07 17:20:11 +0300

    naive implementation of clump_finding_problem

--- src/bin/frequent_words.rs
+++ src/bin/frequent_words.rs
@@ -1,29 +1,6 @@
 extern crate bio;
 
-use bio::strings::pattern_count;
-
-fn frequent_words(text: &str, k: usize) -> Vec<&str> {
-    let len = text.len() - k + 1;
-    let mut res = Vec::new();
-    let mut count = vec![0; len];
-    // compute counts
-    for i in 0..len {
-        let pat = &text[i..i+k];
-        count[i] = pattern_count(text, pat);
-    }
-    // find maximum count
-    let max_count = *count.iter().max().unwrap();
-    // read patterns with maximum count
-    for i in 0..len {
-        if count[i] == max_count {
-            res.push(&text[i..i+k]);
-        }
-    }
-    // reduplicate and return
-    res.sort();
-    res.dedup();
-    res
-}
+use bio::strings::frequent_words;
 
 /// 1.2 Hidden Messages in the Replication Origin
 ///
--- src/strings.rs
+++ src/strings.rs
@@ -19,3 +19,49 @@ pub fn indexes(text: &str, pat: &str) -> Vec<usize> {
 pub fn pattern_count(text: &str, pat: &str) -> usize {
     self::indexes(text, pat).len()
 }
+
+fn frequent_words_count(text: &str, k: usize) -> Vec<usize> {
+    let len = text.len() - k + 1;
+    let mut count = vec![0; len];
+    // compute counts
+    for i in 0..len {
+        let pat = &text[i..i+k];
+        count[i] = self::pattern_count(text, pat);
+    }
+    count
+}
+
+pub fn frequent_words(text: &str, k: usize) -> Vec<&str> {
+    let mut res = Vec::new();
+    let count = frequent_words_count(text, k);
+    // find maximum count
+    let max_count = *count.iter().max().unwrap();
+    // read patterns with maximum count
+    for i in 0..count.len() {
+        if count[i] == max_count {
+            res.push(&text[i..i+k]);
+        }
+    }
+    // reduplicate and return
+    res.sort();
+    res.dedup();
+    res
+}
+
+pub fn clump_finding_naive(text: &str, k: usize, l: usize, t: usize) -> Vec<&str> {
+    let len = text.len() - l + 1;
+    let mut res = Vec::new();
+    for i in 0..len {
+        let chunk = &text[i..i+l];
+        let count = self::frequent_words_count(chunk, k);
+
+        for j in 0..count.len() {
+            if count[j] == t {
+                res.push(&chunk[j..j+k]);
+            }
+        }
+    }
+    res.sort();
+    res.dedup();
+    res
+}
